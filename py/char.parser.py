fa86e3f18900acdc1262b0f76515bf05804cf9a1
ZoeLeBlanc <zgleblanc@gmail.com>
2018-01-09 14:43:15 -0500

    scripts working

--- py/char.parser.py
+++ py/char.parser.py
@@ -1,42 +1,54 @@
 #!/usr/bin/env python
-#char.parser.py 
+#char.parser.py
 #Ciera Martinez
 
 #This script takes in two arguments
 #1. text file (sampleText)
 #2. List of terms to seach text file (listOfTerms)
 # and then prints all terms and their postion in text file
-# outputs results into ./data directory "charList.txt" 
+# outputs results into ./data directory "charList.txt"
 
 #sample command to run (for infinite jest project)
 #python py/char.parser.py data/bookText/David-Foster-Wallace-Infinite-Jest-v2.0.txt data/character/characters.mod.txt
 
 import re
-import sys	
+import sys
+import pandas as pd
+import io
 
-#This sets up output file
-orig_stdout = sys.stdout
-data = open("./data/pyOutputs/characterPosition.txt", 'w')
-sys.stdout = data
 
-#prints the headers of columns
-print 'term\tposition'
-
-sampleText = open(sys.argv[1]) #file that contains text
+sampleText = io.open(sys.argv[1], mode='r', encoding='utf-8') #file that contains text
 listOfTerms = open(sys.argv[2]) #file that contains terms to search for
 
 sampleRead = sampleText.read() #Makes one item string
-termRead = listOfTerms.read() 
+termRead = listOfTerms.read()
+termSplit = termRead.splitlines() #split by new line
+df_chapters = pd.DataFrame(columns=['chapter', 'term', 'position'])
 
-termSplit = termRead.split("\r") #split by new line
 
+current_pos = 0
 for term in termSplit:
-	for m in re.finditer(term, sampleRead):
-		print m.group(0), "\t", m.start()
+	for match in re.finditer("<ch><\d\d>", sampleRead):
+		next_pos=match.end()
+		current_text = sampleRead[current_pos:next_pos]
+		for m in re.finditer(term, current_text):
+			if m:
+				print('position',match.group(0),m.group(0), m.span())
+				df_chapters.loc[len(df_chapters)] = [match.group(0),m.group(0), m.span()]
 
+			else:
+				pass
+				# return null, null
+		current_pos=next_pos
+print(df_chapters)
+df_char_count = df_chapters.groupby(['chapter', 'term']).size().reset_index(name='counts')
+print(df_char_count)
+df_chapters.to_csv('samantha_characters_position_chapters.csv')
+df_char_count.to_csv('samantha_characters_counts_chapters.csv')
+# print(dict_chapters)
 #Outputs print
-sys.stdout = orig_stdout
+# sys.stdout = orig_stdout
 
 sampleText.close()
 listOfTerms.close()
-data.close()
+# data.close()
